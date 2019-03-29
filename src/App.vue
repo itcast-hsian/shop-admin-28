8fa3e947a46801189661a19c5670840fae8af4db
itcast-hsian <43436182+itcast-hsian@users.noreply.github.com>
2019-03-29 08:25:03 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    hello vue
+    <p>
+      <el-button type="primary">主要按钮</el-button>
+    </p>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  mounted(){
+    
+    // 调用axios请求数据,axios的成功的回调要是.then这个方法来调用
+    this.$axios({
+      method: "GET",
+      url: "https://api.github.com/users",
+    }).then(res => {
+      console.log(res)
+    })
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,45 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入axios
+import axios from "axios";
 
-Vue.config.productionTip = false
+// ElementUI: 1.引入element-ui
+import ElementUI from  "element-ui";
+// ElementUI: 2.element-ui样式导入
+import 'element-ui/lib/theme-chalk/index.css';
 
+// ElementUI: 3.全局注册插件
+Vue.use(ElementUI);
+
+// 当前是开发环境,可以忽略
+Vue.config.productionTip = false;
+
+// 挂载vue构造函数的原型链，每个组件可以通过this.$axios来调用
+Vue.prototype.$axios = axios
+
+// 根实例
+// $mount('#app')相当于指定el的配置
+// render 是es6箭头函数的缩写
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+
+
+
+
+
+
+// --------------------------------------------
+// 之前的根实例
+// new Vue({
+//   el: "#app",
+
+//   render: function(createElement){
+//     return createElement(App)
+//   },
+
+//   // 箭头函数只有一行代码并且需要返回的时候
+//   render: h = h(App)
+// })
